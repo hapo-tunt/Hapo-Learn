395f18590bec4ec6276ec2a21c98ef418c6d1bb3
hapo-tunt <93267546+hapo-tunt@users.noreply.github.com>
2022-01-07 17:02:46 +0700

    fix lines

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('{home}', [App\Http\Controllers\HomeController::class, 'index'])->where('home', 'home|')->name('home.index');
 
 Route::group(['namespace' => 'App\Http\Controllers\Auth'], function () {
-    Route::group(['middleware' => ['guest']], function() {
+    Route::group(['middleware' => ['guest']], function () {
         Route::get('/register', 'RegisterController@show')->name('register.show');
         Route::post('/register', 'RegisterController@register')->name('register.perform');
 
